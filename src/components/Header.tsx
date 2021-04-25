1eac61462f7bdd697db87e60c94d84d1c3a1ca41
Higor Cerqueira dos Santos <higorcsantos@MacBook-Pro-de-Higor.local>
2021-04-25 13:08:14 -0300

    Plant Select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,19 @@ import {
     Text,
     Image
 } from "react-native";
-import colors from "../styles/colors"
+import colors from "../styles/colors";
+import {getStatusBarHeight} from "react-native-iphone-x-helper";
+import avatar from "../assets/avatar.jpg";
+import fonts from "../styles/fonts";
 
 export function Header(){
     return(
         <View style = {styles.container}>
-
+            <View>
+                <Text style={styles.greeting}>Olá, </Text>
+                <Text style={styles.userName}>Higor</Text>
+            </View>
+            <Image source={avatar} style={styles.image}/>
         </View>
     )
 };
@@ -19,9 +26,26 @@ const styles = StyleSheet.create({
     container: {
         width: "100%",
         flexDirection: "row",
-        backgroundColor: colors.red,
         paddingVertical: 20,
         alignItems: "center",
-        justifyContent: "space-between"
+        justifyContent: "space-between",
+        marginTop: getStatusBarHeight(),
+
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 40
     }
 })
--- src/screens/PlantSelect.tsx
+++ src/screens/PlantSelect.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,FlatList } from "react-native";
+import { color } from "react-native-reanimated";
+import { EnviromentButton } from "../components/EnviromentButton";
 import { Header } from "../components/Header";
 import colors from "../styles/colors"
+import fonts from "../styles/fonts";
 
 export function PlantSelect(){
     return(
         <View style={styles.container}>
+            <View style={styles.header}>
             <Header/>
-            <Text>Selecionar Planta</Text>
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}> 
+                    voce quer colocar sua planta
+                </Text>
+            </View>
+            <FlatList
+            data={[1,2,3,4,5]}
+            renderItem={({item}) => (
+                <EnviromentButton title="Conzinha" active/>
+            )}/>
+            
         </View>
     )
 }
@@ -16,5 +32,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.background,
+    },
+    title: {
+        fontSize: 17,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
+    },
+    header: {
+        paddingHorizontal: 20
     }
 })
